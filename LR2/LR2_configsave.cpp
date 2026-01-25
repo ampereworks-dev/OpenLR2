ampereworks-dev/OpenLR2

commit 450a7983f75ae75a4f5f3272f88c498a31f99d5f

fix: fix some memory leaks

LR2/LR2_configsave.cpp
@@ -92,6 +92,8 @@ int ReadKeyConfig(game *game, const char *FilePath) {
 	ReadXml_Int_Multi("keyconfig", "key32", "id", (game->config).input.buttonMap[0x20], hXml);
 	ReadXml_Int_Multi("keyconfig", "key33", "id", (game->config).input.buttonMap[0x21], hXml);
 
+	delete hXml;
+
 	return 1;
 }
 
@@ -890,9 +892,7 @@ int ReadSkinCustomize(SkinUser *sku, char *FilePath) {
 	////0~39 repeat
 	//ReadXml_Str("skincustomize", "customfile", "filename_0", CSTR(""), &sku->customize_filename[0], hXml);
 
-	if (hXml) {
-		delete(hXml);
-	}
+	delete(hXml);
 
 	return 1;
 }

LR2/En_fileutil.cpp
@@ -17,26 +17,6 @@
 //437c90
 //437cf0
 int makeFileHash(LPCSTR filepath, LPCSTR oBuf) {
-	//HANDLE hFile;
-	//hFile = CreateFileA(filepath, GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN | FILE_ATTRIBUTE_NORMAL, NULL);
-	//if (hFile == (HANDLE)-1) return -1;
-
-	////not original code
-	//BY_HANDLE_FILE_INFORMATION FileInformation;
-	//GetFileInformationByHandle(hFile, &FileInformation);
-	//unsigned int size = FileInformation.nFileSizeLow;
-	//char* buffer = (char*)malloc(size);
-	//DWORD readsize;
-	//if (buffer != NULL) {
-	//	ReadFile(hFile, buffer, size, &readsize, 0);
-	//	CloseHandle(hFile);
-	//	
-	//	MD5byte(&buffer, readsize, (char*)oBuf);
-
-	//	free(buffer);
-	//}
-	//return 1;
-
 	FILE* pFile;
 	pFile = fopen(filepath, "rb");
 	if (!pFile)  return -1;
@@ -45,6 +25,9 @@ int makeFileHash(LPCSTR filepath, LPCSTR oBuf) {
 
 	sprintf((char*)oBuf, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", md5buf[0], md5buf[1], md5buf[2], md5buf[3],
 		md5buf[4], md5buf[5], md5buf[6], md5buf[7], md5buf[8], md5buf[9], md5buf[10], md5buf[11], md5buf[12], md5buf[13], md5buf[14], md5buf[15]);
+
+	free(md5buf);
+
 	return 1;
 }
 

LR2/LR2_bmsload.cpp
@@ -478,7 +478,7 @@ int LoadBmsResource(gameplay *gp, CSTR /*BMSfilepath*/, AUDIO *aud, ConfigStruct
 
 	int Rtmp, Gtmp, Btmp;
 
-	ErrorLogAdd("BMSの画像とサウンドをロードします");
+	ErrorLogAdd("BMSの画像とサウンドをロードします\n");
 
 	if ((cfg->play).autojudge == 2) { //silent
 		gp->loadObject_loaded = gp->loadObject_total;

LR2/LR2_ir.cpp
@@ -503,6 +503,7 @@ CSTR UrlEncode(CSTR in) {
 
 //4bb820
 RANKING::RANKING() {
+	// FIXME: replace with std::vector. Currently this leaks memory.
 	ranking = (RANKINGPLAYER*)malloc(sizeof(RANKINGPLAYER) * 1000);
 	assert(ranking != nullptr);
 	rankingCount = 0;

LR2/LR2_skinmanage.cpp
@@ -175,6 +175,7 @@ int SetFirstSkins(game *g){
 
 //4a7390
 int InitSkinData(SkinManage *skm){
+	// FIXME: 1) move to SkinManage constructor 2) move to std::vector. This leaks memory.
 	skm->Max = 100;
 	skm->Data = (SkinHeader *)malloc(sizeof(SkinHeader) * 100);
 	assert(skm->Data != nullptr);

LR2/Scene02_Songselect.cpp
@@ -3028,6 +3028,7 @@ int SwapBmsList(SONGSELECT *ss){
 
 //444650
 int InitBmsList(SONGSELECT *ss) {
+	// FIXME: 1) move to SONGSELECT constructor 2) move to std::vector. This leaks memory.
 	ss->bmsListSize = 1000;
 	ss->cur = 0;
 	ss->bmsList = (SONGDATA*)malloc(sizeof(SONGDATA) * 1000);

LR2/strclass.cpp
@@ -488,9 +488,7 @@ bool CSTR::canOpenFile() {
 
 //43b630
 CSTR::CSTR() {
-	body = NULL;
 	body = (char *)calloc(1, 0x40);
-	return;
 }
 
 //43b650 copy construct

commit 25044b3cc3569b3edd581e5102b30d340974cc32

style: put 'delete' in braces
Fixes: 450a7983f75a ("fix: fix some memory leaks")

LR2/LR2_configsave.cpp
@@ -92,7 +92,7 @@ int ReadKeyConfig(game *game, const char *FilePath) {
 	ReadXml_Int_Multi("keyconfig", "key32", "id", (game->config).input.buttonMap[0x20], hXml);
 	ReadXml_Int_Multi("keyconfig", "key33", "id", (game->config).input.buttonMap[0x21], hXml);
 
-	delete hXml;
+	delete(hXml);
 
 	return 1;
 }